de2e658d81553088dd12b2d4a3d812c612014b74
Andrew Metcalfe <andrew.metcalfe@artificialgold.com>
2013-07-31 07:45:30 +0100

    Removed type

--- CMZeroAPI/UnitTests/Api/CollectionControllerTests.cs
+++ CMZeroAPI/UnitTests/Api/CollectionControllerTests.cs
@@ -206,7 +206,7 @@ namespace UnitTests.Api
                 }
             }
 
-            [Test]3
+            [Test]
             public void it_should_return_HttpResponseException_with_status_code_bad_request()
             {
                 exception.Response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
